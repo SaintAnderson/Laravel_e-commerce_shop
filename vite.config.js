64768b99ccb6cb0487a3cebda06ab6c98b0b1a6d
Igor Kolomoytsev <i.kolomoytsev@vk.team>
2024-02-05 20:01:42 +0300

    3 - Перенос шрифтов, картинок, стилей и скриптов в проект. Удален файл gulpfile.js. Удалены файлы с раширением styl. Поправвлен Файл vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/basic.css',
+                'resources/css/extra.css',
+                'resources/css/fonts.css',
+                'resources/js/app.js',
+                'resources/js/bootstrap.js',
+                'resources/js/scripts.js'
+            ],
             refresh: true,
         }),
     ],

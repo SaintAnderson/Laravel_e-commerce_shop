d74e15b685b146fff7e073f4ddf5b436f2bc194e
Ruslan Mekhtiev <mekhtievrs0@gmail.com>
2024-02-08 16:12:25 +0300

    fix: small fix

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,8 +1,2 @@
 <link href="favicon.ico" rel="shortcut icon">
-<link rel="preload" href="assets/fonts/Roboto/Roboto-Regular.woff" as="font">
-<link rel="preload" href="assets/fonts/Roboto/Roboto-Italic.woff" as="font">
-<link rel="preload" href="assets/fonts/Roboto/Roboto-Bold.woff" as="font">
-<link rel="preload" href="assets/fonts/Roboto/Roboto-Bold_Italic.woff" as="font">
-<link rel="preload" href="assets/fonts/Roboto/Roboto-Light.woff" as="font">
-<link rel="preload" href="assets/fonts/Roboto/Roboto-Light_Italic.woff" as="font">
 @vite('resources/css/app.css')

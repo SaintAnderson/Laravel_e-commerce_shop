bf28d4b64e1e1d9c3b4a7c5d3bb0fb8af6382f26
Ruslan Samburov <samburov.com@gmail.com>
2024-02-15 01:51:58 +0300

    fixed

--- resources/views/components/products/product-card.blade.php
+++ resources/views/components/products/product-card.blade.php
@@ -10,6 +10,12 @@
             <div class="Card-category">
                 {{ $category }}
             </div>
+            <div class="Card-hover"><a class="Card-btn" href="#"><img
+                src="assets/img/icons/card/bookmark.svg" alt="bookmark.svg" /></a><a
+            class="Card-btn" href="#"><img src="assets/img/icons/card/cart.svg"
+                alt="cart.svg" /></a><a class="Card-btn" href="compare.html"><img
+                src="assets/img/icons/card/change.svg" alt="change.svg" /></a>
+            </div>
         </div>
     </div>
     @if ($sale)

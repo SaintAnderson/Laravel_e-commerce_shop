e033978df04224fa8a3f2e44ae6c03ddf8f584c0
Igor Kolomoytsev <i.kolomoytsev@vk.team>
2024-02-19 16:32:14 +0300

    30 - Правки в ходе МР.

--- resources/views/components/popular-products-block.blade.php
+++ resources/views/components/popular-products-block.blade.php
@@ -5,25 +5,7 @@
     </header>
     <div class="Cards">
         @foreach($popularProducts as $product)
-            <div class="Card"><a class="Card-picture" href="#"><img src="assets/img/content/home/card.jpg"
-                                                                    alt="card.jpg" /></a>
-                <div class="Card-content">
-                    <strong class="Card-title"><a href="#">{{ $product->title }}</a>
-                    </strong>
-                    <div class="Card-description">
-                        <div class="Card-cost">
-                            @if($product->priceOld)<span class="Card-priceOld">{{ "$" . $product->priceOld }}</span>@endif
-                            <span class="Card-price">{{  "$" . $product->price }}</span>
-                        </div>
-                        <div class="Card-hover"><a class="Card-btn" href="#"><img
-                                    src="assets/img/icons/card/bookmark.svg" alt="bookmark.svg" /></a><a
-                                class="Card-btn" href="#"><img src="assets/img/icons/card/cart.svg"
-                                                               alt="cart.svg" /></a><a class="Card-btn" href="compare.html"><img
-                                    src="assets/img/icons/card/change.svg" alt="change.svg" /></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @include('components.products.product-card', ['product' => $product])
         @endforeach
     </div>
 </div>
--- resources/views/components/products/product-card.blade.php
+++ resources/views/components/products/product-card.blade.php
@@ -1,26 +1,19 @@
-<div class="Card"><a class="Card-picture" href="{{ route('products.show', ['slug' => $product->slug]) }}"><img src="/assets/img/content/home/card.jpg" alt="Загрузка..." /></a>
+<div class="Card"><a class="Card-picture" href="#"><img src="assets/img/content/home/card.jpg"
+                                                        alt="card.jpg" /></a>
     <div class="Card-content">
-        <strong class="Card-title"><a href="{{ route('products.show', ['slug' => $product->slug]) }}">{{ $product->title }}</a>
+        <strong class="Card-title"><a href="#">{{ $product->title }}</a>
         </strong>
         <div class="Card-description">
             <div class="Card-cost">
-                {{-- @if ($sale) <span class="Card-priceOld">${{ $product->price }}</span> @endif --}}
-                <span class="Card-price">${{ $product->price }}</span>
-            </div>
-            <div class="Card-category">
-                @if($product->category) {{ $product->category->name }} @endif
+                @if($product->old_price)<span class="Card-priceOld">{{ "$" . $product->old_price }}</span>@endif
+                <span class="Card-price">{{  "$" . $product->price }}</span>
             </div>
             <div class="Card-hover"><a class="Card-btn" href="#"><img
-                src="/assets/img/icons/card/bookmark.svg" alt="bookmark.svg" /></a><a
-            class="Card-btn" href="#"><img src="/assets/img/icons/card/cart.svg"
-                alt="cart.svg" /></a><a class="Card-btn" href="#"><img
-                src="/assets/img/icons/card/change.svg" alt="#" /></a>
+                        src="assets/img/icons/card/bookmark.svg" alt="bookmark.svg" /></a><a
+                    class="Card-btn" href="#"><img src="assets/img/icons/card/cart.svg"
+                                                   alt="cart.svg" /></a><a class="Card-btn" href="compare.html"><img
+                        src="assets/img/icons/card/change.svg" alt="change.svg" /></a>
             </div>
         </div>
     </div>
-    {{-- @if ($sale)
-        <div class="Card-sale">
-            -{{ $sale }}%
-        </div>
-    @endif --}}
 </div>

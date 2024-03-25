f80c70e20fe7ddd56eeed0d30f0840a6a69f0e8f
Ruslan Mekhtiev <mekhtievrs0@gmail.com>
2024-03-25 12:11:23 +0300

    fix: small about page fix

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -6,12 +6,12 @@
         <div class="Middle-top">
             <div class="wrap">
                 <div class="Middle-header">
-                    <h1 class="Middle-title">О {{ $title }}
+                    <h1 class="Middle-title">О нас
                     </h1>
                     <ul class="breadcrumbs Middle-breadcrumbs">
-                        <li class="breadcrumbs-item"><a href="index.html">home</a>
+                        <li class="breadcrumbs-item"><a href="index.html">Главная</a>
                         </li>
-                        <li class="breadcrumbs-item breadcrumbs-item_current"><span>About Us</span>
+                        <li class="breadcrumbs-item breadcrumbs-item_current"><span>О нас</span>
                         </li>
                     </ul>
                 </div>

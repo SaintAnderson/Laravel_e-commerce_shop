c97ed7cf3465161e830d9bb4f71eef6550a376fd
raufdzhon_aliboev <raufjonaliboev77@gmail.com>
2024-03-25 18:23:45 +0900

    #75: Added link to category-cart

--- resources/views/components/categories/category-card.blade.php
+++ resources/views/components/categories/category-card.blade.php
@@ -1,4 +1,4 @@
-<a class="BannersHomeBlock" href="#">
+<a class="BannersHomeBlock" href="{{ route('catalog.category', $category)}}">
     <div class="BannersHomeBlock-row">
         <div class="BannersHomeBlock-block">
             <strong class="BannersHomeBlock-title">
@@ -6,13 +6,12 @@
             </strong>
             <div class="BannersHomeBlock-content">from&#32;
                 <span class="BannersHomeBlock-price">
-{{--                    {{// todo Минимальная цена в данной категории}}--}}
+                    {{-- {{// todo Минимальная цена в данной категории}}--}}
                 </span>
             </div>
         </div>
         <div class="BannersHomeBlock-block">
-            <div class="BannersHomeBlock-img"><img src="assets/img/content/home/videoca.png"
-                                                   alt="videoca.png" />
+            <div class="BannersHomeBlock-img"><img src="assets/img/content/home/videoca.png" alt="videoca.png" />
             </div>
         </div>
     </div>
